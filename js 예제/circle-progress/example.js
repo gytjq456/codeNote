0372fc3e9178b157041dcfb4ee444244edc085bb
gytjq456 <53968123+gytjq456@users.noreply.github.com>
2021-05-11 17:55:47 +0900

    Update example.js

--- js 예제/circle-progress/example.js
+++ js 예제/circle-progress/example.js
@@ -1,25 +1,47 @@
-	$(".circle").each(function(){
-		var val = parseInt($(this).data("value"));
+var experience = $("#experience")
+	circle(0)
+	var ifCircle = false;
+	$(window).scroll(function () {
+		if (experience.hasClass("on")) {
+			if (!ifCircle) {
+				ifCircle = true;
+				circle(1)
+			}
+		}
+
+	})
+
+function circle(pos) {
+	$(".circle").each(function () {
+		var val = 0;
+		if (pos != 0) {
+			val = parseInt($(this).data("value"));
+		}
+		console.log(val)
 		var degVal = 360 * val / 100;
-		console.log(degVal)
+
+
 		$(this).find("p").stop().animate({
-			'transform':'rotate('+(-degVal)+'deg)'
-		},1500)
-		
+			'transform': 'rotate(' + (-degVal) + 'deg)'
+		}, 1500)
+
+
 		$(this).circleProgress({
-			value: val/100,
-			startAngle:-Math.PI/2 ,
-			size:260,
-			fill:{
-				gradient:["#fff","#fff"],
+			value: val / 100,
+			startAngle: -Math.PI / 2,
+			size: 260,
+			fill: {
+				gradient: ["#fff", "#fff"],
 				gradientAngle: Math.PI / 2
 			},
-			animation:{
-				duration:1500,
-				easing:"swing"
+			animation: {
+				duration: 1500,
+				easing: "swing"
 			},
-			lineCap : "butt",
-			thickness:1,
-			reverse:true,
-			emptyFill: "rgba(255,255,255,0.1)",		
+			lineCap: "butt",
+			thickness: 1,
+			reverse: true,
+			emptyFill: "rgba(255,255,255,0.1)",
 		});
+	})
+}
