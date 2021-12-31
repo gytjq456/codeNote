a8593185922628be71a0e3dc46f2cfb9c83249e9
gytjq456 <53968123+gytjq456@users.noreply.github.com>
2021-12-31 12:01:01 +0900

    Add files via upload

---
+++ js 예제/내가만든 제이쿼리 라이브러리/common.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+
+	// 플러그인 호출 
+	$("a").greenify({
+		color: "orange"
+	});
+
+
+	// header menu event
+	let gnb = $("#gnb")
+	$(window).on("load resize", function () {
+		let window_width = $(window).width();
+		if (window_width > 1200) {
+			gnb.find(".depth1 > li > a").off("click");
+			gnb.find(".depth1 > li").off().on("mouseenter mouseleave", function (e) {
+				// $(this).gnbHoverEvent(e, "fade");
+				// $(this).gnbHoverEvent(e, "slide");
+				$(this).gnbHoverEvent(e, "allSlide", 300);
+			})
+		} else {
+			gnb.find(".depth1 > li").off("mouseenter mouseleave");
+			gnb.find(".depth1 > li > a").off().on("click", function (e) {
+				e.preventDefault();
+				$(this).gnbHoverEvent(e, "slide");
+			})
+		}
+	})
+
+
+	// tab menu click / content change
+	let tab = $(".tab");
+	tab.find("a").on("click", function () {
+		$(this).tabChange(".tabContent");
+	})
+
+	// toggleTab menu click event
+	let toggleTabDiv = $(".toggleTab");
+	toggleTabDiv.find("button").on("click", function () {
+		$(this).toggleTab(".txtBox");
+	})
+
+})
